29024d580ec58ec4d76b1b46a01ea8ac70af0838
Andrew Rutherford <sentrakh@gmail.com>
2019-11-03 23:47:59 +1100

    Initial Typescript/rewrite commit'

---
+++ app.ts
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+import * as discord from 'discord.js';
+import * as Axios from 'axios';
+import * as sqlite3_i from 'sqlite3';
+import {config} from './config';
+import {DatabaseHelper} from './database-helper';
+import {ServerHandler} from './server';
+
+const sqlite3 = sqlite3_i.verbose();
+const db = new sqlite3.Database('cosmobot.db');
+const dbh = new DatabaseHelper(db);
+
+db.serialize(async ()=>{
+    await dbh.run(`CREATE TABLE IF NOT EXISTS Bio (
+        DiscordID TEXT NOT NULL,
+        Description TEXT,
+        Quote TEXT,
+        Job TEXT
+    )`);
+    await dbh.run(`CREATE TABLE IF NOT EXISTS AvailableRoles (
+        RoleID TEXT NOT NULL
+    )`)
+});
+
+const axios = Axios.default;
+
+const client = new discord.Client();
+
+client.on('message',(msg)=>{
+    
+    // Throw out message if it doens't start with the prefix
+    if (msg.content[0] !== config.prefix) {
+        return;
+    }
+    get_roles();
+
+});
+
+client.login(process.env.TOKEN);
+
+const get_roles = () => {
+    const guild = client.guilds.first();
+    const roles = guild.roles.array();
+    const role_list = [];
+    for (const role of roles) {
+        role_list.push({id:role.id, name:role.name, color: (role.hexColor == '#000000' ? undefined : role.hexColor)});  // undefined means no color
+    }
+    //console.log(role_list);
+    return role_list;
+}
+const get_role = (role_id: string) => {
+    const guild = client.guilds.first();
+    const role = guild.roles.find(r => r.id == role_id);
+    if (role != null) {
+        return {id:role.id, name:role.name, color: (role.hexColor == '#000000' ? undefined : role.hexColor)};
+    } else {
+        return null;
+    }
+}
+
+const is_mod = async (user_id:string) => {
+    const guild = client.guilds.first();
+    const user = await guild.fetchMember(user_id);
+    if (user.roles.find(r=>r.id == process.env.MOD_ROLE_ID)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+const server = new ServerHandler(dbh,{
+    get_roles:get_roles,
+    get_role:get_role,
+    is_mod:is_mod
+});
+
+
---
+++ server/index.ts
@@ -0,0 +1,131 @@
+import * as express from 'express';
+import * as body_parser from 'body-parser';
+import * as cookie_parser from 'cookie-parser';
+import * as passport from 'passport';
+import * as session from 'express-session';
+import {Strategy} from 'passport-discord';
+import {DatabaseHelper} from '../database-helper';
+export class ServerHandler {
+    constructor(dbh: DatabaseHelper, g:any) {
+        const db = dbh.get_db();
+        const app = express();
+
+        app.use(cookie_parser());
+        app.use(body_parser.json());
+        app.use(body_parser.urlencoded({extended:false}));
+        app.use(session({secret:process.env.EXPRESS_SECRET,resave:true,saveUninitialized:true}));
+        app.use(passport.initialize());
+        app.use(passport.session());
+        app.use('/assets',express.static('./assets'));
+        app.set('view engine', 'ejs');
+
+        const active_sessions = {};
+
+        const auth_strategy = new Strategy ({
+                clientID:process.env.CLIENT_ID,
+                clientSecret:process.env.SECRET_ID,
+                callbackURL:process.env.CALLBACK_URL
+            },
+            (access_token,refresh_token,profile,cb)=>{
+                //@ts-ignore
+                profile.refresh_token = refresh_token; 
+                //@ts-ignore
+                profile.access_token = access_token;
+                const avatar_base = 'https://cdn.discordapp.com';
+                let avatar_url = `${avatar_base}`;
+                if (profile.avatar) {
+                    avatar_url += `/avatars/${profile.id}/${profile.avatar}.${(profile.avatar.includes('a_')?'gif':'png')}`
+                } else {
+                    avatar_url += `/embed/avatars/${parseInt(profile.discriminator) % 5}.png`;
+                }
+                //@ts-ignore
+                profile.avatar_url = avatar_url;
+                cb(null,profile);
+            }
+        );
+
+        passport.use(auth_strategy);
+        passport.serializeUser((user:any,done)=>{
+            active_sessions[user.id] = user;
+            done(null,user);
+        });
+        passport.deserializeUser((user:any,done)=>{
+            done(null,active_sessions[user.id]);
+        });
+        app.use('*',async (req,res,next)=>{
+            if (req.user) {
+                //@ts-ignore
+                req.user.mod = await g.is_mod(req.user.id);
+            }
+            next();
+        });
+        const mod_check = (req,res,next) => {
+            if (req.user) {
+                //@ts-ignore
+                if (req.user.mod === true) {
+                    return next();
+                }
+            }
+            res.redirect('/');
+        }
+
+        // passport/login
+        app.get('/login',(req,res)=>{
+            res.redirect(process.env.AUTH_URL);
+        });
+        app.get('/logout',(req,res)=>{
+            if (req.user == undefined) {
+                res.redirect('/login');
+            } else {
+                req.logout();
+                res.redirect('/');
+            }
+        });
+        app.get('/callback', passport.authenticate('discord',{ failureRedirect: '/'}),(req,res)=>{
+            res.redirect('/');
+        });
+
+        app.get('/',(req,res)=>{
+            res.render('page',{page:'index',user:req.user});
+        });
+
+        app.get('/roles',mod_check,async (req,res)=>{
+            const available_roles = [];
+            const result = await dbh.all(`SELECT * FROM AvailableRoles`);
+            for (let i=0;i<result.length;i++) {
+                const role = g.get_role(result[i].RoleID);
+                available_roles.push(role);
+            }
+            res.render('page',{page:'roles',role_list:g.get_roles(),available_roles:available_roles,query:req.query,user:req.user});
+        });
+        app.post('/role/add',mod_check,async (req,res)=>{
+            const role_id = req.body.role_id;
+            const stmt_check = db.prepare(`SELECT * FROM AvailableRoles WHERE RoleID=(?)`);
+            if ((await dbh.stmt_all(stmt_check,role_id)).length > 0) {
+                return res.redirect('/roles?error=1')   // role already available
+            }
+            const role = g.get_role(role_id);
+            if (role == null) {
+                return res.redirect('/roles?error=2');  // role doesn't exist
+            }
+            const stmt = db.prepare(`INSERT INTO AvailableRoles (RoleID) VALUES (?)`);
+            await dbh.stmt_run(stmt,role_id);
+            res.redirect('/roles?success=1');
+        });
+        app.post('/role/delete',mod_check,async (req,res)=>{
+            const role_id = req.body.role_id;
+            const stmt_check = db.prepare(`SELECT * FROM AvailableRoles WHERE RoleID=(?)`);
+            if ((await dbh.stmt_all(stmt_check,role_id)).length == 0) {
+                return res.redirect('/roles?error=3')   // role not available
+            }
+            const role = g.get_role(role_id);
+            if (role == null) {
+                return res.redirect('/roles?error=2');  // role doesn't exist
+            }
+            const stmt = db.prepare(`DELETE FROM AvailableRoles WHERE RoleID = (?)`);
+            await dbh.stmt_run(stmt,role_id);
+            res.redirect('/roles?success=2');
+        });
+        app.listen(process.env.PORT);
+    }
+}
